36dfcdb929b516f6b45225dd781d27d73f920cd2
Bocchi-Dev <47100935+Bocchi-Dev@users.noreply.github.com>
2021-11-07 22:15:15 +0800

    final movement and interaction system fixed

--- ClassroomSimulator/Assets/Scripts/Controls.cs
+++ ClassroomSimulator/Assets/Scripts/Controls.cs
@@ -9,9 +9,7 @@ public class Controls : MonoBehaviour
 
     public float moveSpeed = 10f;
 
-    public float gravity = 20f;
-
-    public bool isGrounded = true;
+    public float gravity = 10f;
 
     //Interaction Jazz
     public int distanceOfRaycast;
@@ -30,7 +28,20 @@ public class Controls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRaycast))
+        {
+            if (Input.GetButtonDown("Interact") && _hit.transform.CompareTag("Rotateable"))
+            {
+                _hit.transform.gameObject.GetComponent<RotateObject>().ChangeSpin();
+            }
+            if (_hit.transform.CompareTag("Button") && Input.GetButtonDown("Interact"))
+            {
+                _hit.transform.gameObject.GetComponent<ChangeScene>().changeScene(_hit.transform.gameObject.GetComponent<ChangeScene>().SceneName);
+                Debug.Log("touched " + _hit.transform.gameObject.GetComponent<ChangeScene>().SceneName);
+            }
+        }
         playerMovement();
     }
 
@@ -39,32 +50,30 @@ public class Controls : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
+        //transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * moveSpeed * Time.deltaTime);
         Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
         Vector3 velocity = direction * moveSpeed;
         velocity = Camera.main.transform.TransformDirection(velocity);
+        velocity.y -= gravity;
+
+        //Vector3 move = transform.right * horizontalInput + transform.forward * verticalInput;
 
         controller.Move(velocity * Time.deltaTime);
     }
-
     void interact()
     {
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
 
         if (Physics.Raycast(ray, out _hit, distanceOfRaycast))
         {
-            if (Input.GetButtonDown("Interact"))
+            if (_hit.transform.CompareTag("Rotateable") && Input.GetButtonDown("Interact"))
             {
-                if (_hit.transform.CompareTag("Rotateable"))
-                {
-                    _hit.transform.gameObject.GetComponent<RotateObject>().ChangeSpin();
-                }
-                if (_hit.transform.CompareTag("Button"))
-                {
-                    _hit.transform.gameObject.GetComponent<ChangeScene>().changeScene(_hit.transform.gameObject.GetComponent<ChangeScene>().SceneName);
-                    Debug.Log("touched " + _hit.transform.gameObject.GetComponent<ChangeScene>().SceneName);
-
-                }
-                
+                _hit.transform.gameObject.GetComponent<RotateObject>().ChangeSpin();
+            }
+            if (_hit.transform.CompareTag("Button") && Input.GetButtonDown("Interact"))
+            {
+                _hit.transform.gameObject.GetComponent<ChangeScene>().changeScene(_hit.transform.gameObject.GetComponent<ChangeScene>().SceneName);
+                Debug.Log("touched " + _hit.transform.gameObject.GetComponent<ChangeScene>().SceneName);
             }
         }
     }
