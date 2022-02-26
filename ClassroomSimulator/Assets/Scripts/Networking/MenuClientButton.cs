0bd62d055c396adde3c47890d270b7cb2c437e99
Bocchi-Dev <47100935+Bocchi-Dev@users.noreply.github.com>
2022-02-26 10:16:35 +0800

    changed some button for testing

--- ClassroomSimulator/Assets/Scripts/Networking/MenuClientButton.cs
+++ ClassroomSimulator/Assets/Scripts/Networking/MenuClientButton.cs
@@ -32,4 +32,6 @@ public class MenuClientButton : MonoBehaviour
         playerName = playerNameInput.text;
         manager.StartHost();
     }
+
+   
 }
---
+++ ClassroomSimulator/Assets/Scripts/Networking/TestConnectServerDirectlyHardocoded.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+using TMPro;
+
+public class TestConnectServerDirectlyHardocoded : MonoBehaviour
+{
+    NetworkManager manager;
+
+    void Awake()
+    {
+        manager = FindObjectOfType<NetworkManager>();
+    }
+
+    //This is a temporary function for testing purposes
+    public void connectToServer()
+    {
+        //manager.networkAddress = "sea.playflow.cloud.app";
+        manager.StartClient();
+    }
+}
