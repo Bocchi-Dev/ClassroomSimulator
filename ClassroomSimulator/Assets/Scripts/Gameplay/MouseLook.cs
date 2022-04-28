e79a4f1552400777764ae225d49bfc016a7c3a4c
Bocchi-Dev <47100935+Bocchi-Dev@users.noreply.github.com>
2022-04-28 20:15:04 +0800

    hopefully made some QoL changes

--- ClassroomSimulator/Assets/Scripts/Gameplay/MouseLook.cs
+++ ClassroomSimulator/Assets/Scripts/Gameplay/MouseLook.cs
@@ -26,5 +26,14 @@ public class MouseLook : MonoBehaviour
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetKeyDown("Escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        if(Input.GetMouseButtonDown(1) && Cursor.lockState == CursorLockMode.None)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
